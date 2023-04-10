4b71e4b7f877120075715ea91c9e0d0c7bd431e0
crixstn <crixstn@gmail.com>
2023-04-10 17:02:57 -0300

    feat: get address from CEP

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -29,8 +29,11 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 }
 
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  const cep: string = req.query.cep as string;
+
   try {
-    const address = await enrollmentsService.getAddressFromCEP();
+    const address = await enrollmentsService.getAddressFromCEP({ cep });
+
     res.status(httpStatus.OK).send(address);
   } catch (error) {
     if (error.name === 'NotFoundError') {
